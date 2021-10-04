0fbf9056d7c78f4b978db75cac26fe35166bb354
Mason Holmes <masondaleholmes@outlook.com>
2021-10-04 00:03:22 -0500

    Add project files.

---
+++ ChapterEight8-2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ChapterEight8_2
+{
+    partial class frmTitles
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdTitles = new System.Windows.Forms.DataGridView();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTitles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdTitles
+            // 
+            this.grdTitles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdTitles.Location = new System.Drawing.Point(-2, -3);
+            this.grdTitles.Name = "grdTitles";
+            this.grdTitles.RowTemplate.Height = 25;
+            this.grdTitles.Size = new System.Drawing.Size(679, 408);
+            this.grdTitles.TabIndex = 0;
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.Filter = "SQL Databases (*mdf)|*mdf";
+            // 
+            // frmTitles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdTitles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmTitles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Titles Table";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTitles_FormClosing);
+            this.Load += new System.EventHandler(this.frmTitles_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdTitles)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdTitles;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+    }
+}
+
---
+++ ChapterEight8-2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ChapterEight8_2
+{
+    public partial class frmTitles : Form
+    {
+        SqlConnection booksConnection;
+        SqlCommand titlesCommand;
+        SqlDataAdapter titlesAdapter;
+        DataTable titlesTable;
+        public frmTitles()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTitles_Load(object sender, EventArgs e)
+        {
+            if(dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    booksConnection = new SqlConnection($@"Data Source=.\SQLEXPRESS; AttachDbFilename={Application.StartupPath + "books.mdf"};
+                                                    Integrated Security=True; Connect Timeout=30; User Instance=True");
+                    booksConnection.Open();
+                    //titlesCommand = new SqlCommand("Select * from ")
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void frmTitles_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            booksConnection.Dispose();
+            titlesCommand.Dispose();
+            titlesAdapter.Dispose();
+            titlesTable.Dispose();
+        }
+    }
+}
